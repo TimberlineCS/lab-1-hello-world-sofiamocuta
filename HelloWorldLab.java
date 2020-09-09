babe4197924b244fdd604384924431adb0ed63c9
sofiamocuta <22sofia.mocuta@boiseschools.net>
2020-09-09 14:44:25 +0000

    Stop sign

---
+++ HelloWorldLab.java
@@ -0,0 +1,56 @@
+/*
+Sofia Mocuța
+AP CSA period 2
+08/20/20
+*/
+
+public class HelloWorldLab {
+
+  public static void main(String args[]){
+
+    /*it's a stop sign*/
+
+    System.out.println("        -----------------        ");
+    System.out.println("      /                   \\     ");
+    System.out.println("     /                     \\    ");
+    System.out.println("    /                       \\   ");
+    System.out.println("   /                         \\  ");
+    System.out.println("  /  ____  _____  ____  ____  \\ ");
+    System.out.println(" |  |        |   |    ||    |  | ");
+    System.out.println(" |  |____    |   |    ||____|  | ");
+    System.out.println(" |       |   |   |    ||       | ");
+    System.out.println(" |  _____|   |   |____||       | ");
+    System.out.println(" |                             | ");
+    System.out.println("  \\                           / ");
+    System.out.println("   \\                         /  ");
+    System.out.println("    \\                       /   ");
+    System.out.println("     \\                     /    ");
+    System.out.println("      \\                   /     ");
+    System.out.println("        ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾        ");
+    System.out.println("               ||                ");
+    System.out.println("               ||                ");
+    System.out.println("               ||                ");
+    System.out.println("               ||                ");
+    System.out.println("               ||                ");
+    System.out.println("               ||                ");
+    System.out.println("               ||                ");
+    System.out.println("               ||                ");
+    System.out.println("               ||                ");
+    System.out.println("               ||            _________________           ");
+    System.out.println("               ||          / _________________ \\        ");
+    System.out.println("               ||         / /                 \\ \\      ");
+    System.out.println("               ||        / /                   \\ \\     ");
+    System.out.println("               ||       /  ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾  \\     ");
+    System.out.println("               ||      /     _________________     \\    ");
+    System.out.println("               ||    |‾‾‾‾‾‾                   ‾‾‾‾‾‾|   ");
+    System.out.println("               ||    |                               |   ");
+    System.out.println("               ||   |                                 |  ");
+    System.out.println("               ||   |            __________           |  ");
+    System.out.println("               ||   |           |          |          |  ");
+    System.out.println("               ||   |_________   ‾‾‾‾‾‾‾‾‾‾  _________|  ");
+    System.out.println("               ||     |   |   ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾   |   |    ");
+    System.out.println("    ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|   |                     |   |    ");
+    System.out.println("                      |___|                     |___|    ");      
+  }
+}
+
